5f6622eba622ae47e5a0f7fe05cf5d965b986eb7
Vincent Hilla <vincenthilla5@gmail.com>
2025-02-02 16:06:45 +0100

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+export JAVA_HOME=~/jdk-21.0.1
+export PATH=$JAVA_HOME/bin:$PATH
+
+if ! screen -ls | grep -q "msh-minecraft"; then
+  #./msh.bin
+  screen -dm -S msh-minecraft ./msh.bin
+fi
