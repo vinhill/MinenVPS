5e89c839d55d150be188f3640d2a76ed227e2a98
Vincent Hilla <vincenthilla5@gmail.com>
2025-09-29 22:12:44 +0200

    todos

--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ chmod +x msh.bin
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 tar -xvf jdk-21_linux-x64_bin.tar.gz
 rm jdk-21_linux-x64_bin.tar.gz
+# xxx that is quite fragile, version number and path changes over time, e.g. now subfolder bin must be used
 export JAVA_HOME=~/jdk-21.0.1
 export PATH=$JAVA_HOME/bin:$PATH
 
 # setup forge minecraft server
+# xxx can we store the version somewhere more centralized?
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.21.8-58.1.0/forge-1.21.8-58.1.0-installer.jar
 java -jar forge-1.21.8-58.1.0-installer.jar --installServer
 rm forge-1.21.8-58.1.0-installer.jar
