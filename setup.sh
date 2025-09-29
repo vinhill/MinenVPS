157a1854e0ef46263cdf0e2a2af5e96134fbd120
Vincent Hilla <vincenthilla5@gmail.com>
2025-09-29 21:57:55 +0200

    update version

--- setup.sh
+++ setup.sh
@@ -6,12 +6,15 @@ chmod +x msh.bin
 # setup local installation of java
 wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 tar -xvf jdk-21_linux-x64_bin.tar.gz
+rm jdk-21_linux-x64_bin.tar.gz
 export JAVA_HOME=~/jdk-21.0.1
 export PATH=$JAVA_HOME/bin:$PATH
 
 # setup forge minecraft server
-wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.3.2/forge-1.19.2-43.3.2-installer.jar
-java -jar forge-1.19.2-43.3.2-installer.jar --installServer
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.21.8-58.1.0/forge-1.21.8-58.1.0-installer.jar
+wget 
+java -jar forge-1.21.8-58.1.0-installer.jar --installServer
+rm forge-1.21.8-58.1.0-installer.jar
 echo "eula=true" > eula.txt
 rm user_jvm_args.txt
 # optionally copy mods to ./mods
